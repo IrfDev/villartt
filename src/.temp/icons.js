612b9f79c14ab6c93cbbecb6b642eeec02c7557b
IrfDev <irving.dev@icloud.om>
2020-04-28 20:06:58 -0500

    updating git ignore

--- src/.temp/icons.js
+++ src/.temp/icons.js
@@ -5,37 +5,37 @@ export default {
   "touchicons": [
     {
       "width": 76,
-      "src": "/assets/static/src/favicon.png?width=76&key=907070a"
+      "src": "/assets/static/favicon.f22e9f3.9bb7ffafafc09ac851d81afb65b8ef59.png"
     },
     {
       "width": 152,
-      "src": "/assets/static/src/favicon.png?width=152&key=907070a"
+      "src": "/assets/static/favicon.62d22cb.9bb7ffafafc09ac851d81afb65b8ef59.png"
     },
     {
       "width": 120,
-      "src": "/assets/static/src/favicon.png?width=120&key=907070a"
+      "src": "/assets/static/favicon.1539b60.9bb7ffafafc09ac851d81afb65b8ef59.png"
     },
     {
       "width": 167,
-      "src": "/assets/static/src/favicon.png?width=167&key=907070a"
+      "src": "/assets/static/favicon.dc0cdc5.9bb7ffafafc09ac851d81afb65b8ef59.png"
     },
     {
       "width": 180,
-      "src": "/assets/static/src/favicon.png?width=180&key=907070a"
+      "src": "/assets/static/favicon.7b22250.9bb7ffafafc09ac851d81afb65b8ef59.png"
     }
   ],
   "favicons": [
     {
       "width": 16,
-      "src": "/assets/static/src/favicon.png?width=16&key=fb51670"
+      "src": "/assets/static/favicon.ce0531f.9bb7ffafafc09ac851d81afb65b8ef59.png"
     },
     {
       "width": 32,
-      "src": "/assets/static/src/favicon.png?width=32&key=fb51670"
+      "src": "/assets/static/favicon.ac8d93a.9bb7ffafafc09ac851d81afb65b8ef59.png"
     },
     {
       "width": 96,
-      "src": "/assets/static/src/favicon.png?width=96&key=fb51670"
+      "src": "/assets/static/favicon.b9532cc.9bb7ffafafc09ac851d81afb65b8ef59.png"
     }
   ]
 }
--- src/.temp/now.js
+++ src/.temp/now.js
@@ -1,1 +1,1 @@
-export default 1587776171621
+export default 1587941228827
